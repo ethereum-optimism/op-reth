1cc8a5dcdbd1667205191a10183a9f5bda13a68e
Emilia Hane <elsaemiliaevahane@gmail.com>
2025-01-28 16:15:07 +0100

    Revert redundant predeploy contract address modification since kurtosis proxies address

--- crates/optimism/primitives/src/predeploys.rs
+++ crates/optimism/primitives/src/predeploys.rs
@@ -5,4 +5,4 @@ use alloy_primitives::{address, Address};
 
 /// The L2 contract `L2ToL1MessagePasser`, stores commitments to withdrawal transactions.
 pub const ADDRESS_L2_TO_L1_MESSAGE_PASSER: Address =
-    address!("C0D3C0d3C0d3c0d3C0d3C0D3c0D3c0d3c0D30016");
+    address!("4200000000000000000000000000000000000016");
